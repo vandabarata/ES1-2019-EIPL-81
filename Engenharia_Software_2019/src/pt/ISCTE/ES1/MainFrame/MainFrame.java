3d200be3aa93302352fd97d058f0e73693aed8a1
jmalo1-iscteiul <jmalo1@iscte-iul.pt>
2019-11-07 11:00:08 +0000

    Borders implemented for display

--- Engenharia_Software_2019/src/pt/ISCTE/ES1/MainFrame/MainFrame.java
+++ Engenharia_Software_2019/src/pt/ISCTE/ES1/MainFrame/MainFrame.java
@@ -10,6 +10,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+
 
 public class MainFrame {
 	private JFrame mainFrame;
@@ -20,7 +22,7 @@ public class MainFrame {
 	//TODO: receber por argumento o ficheiro excel
 	public MainFrame() {
 		mainFrame = new JFrame();
-		mainFrame.setLayout(new BorderLayout());
+		mainFrame.setLayout(new BorderLayout(5,5));
 		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		addContents();
 		mainFrame.pack();
@@ -36,13 +38,14 @@ public class MainFrame {
 
 	private void createSouthPanel() {
 		southPanel= new JPanel();
-		southPanel.setLayout(new BorderLayout());
+		southPanel.setBorder(new EmptyBorder(6, 6, 6, 6));
+		southPanel.setLayout(new BorderLayout(5,5));
 		JPanel fileResultsPanel = new JPanel();
-		fileResultsPanel.setLayout(new GridLayout(3,5));
+		fileResultsPanel.setLayout(new GridLayout(3,5,5,5));
 		addContentToFileResultsforPanel(fileResultsPanel);
 		southPanel.add(fileResultsPanel, BorderLayout.CENTER);
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new GridLayout(2,1));;
+		buttonsPanel.setLayout(new GridLayout(2,1,5,5));;
 		addContentToButtonsPanel(buttonsPanel);
 		southPanel.add(buttonsPanel, BorderLayout.EAST);
 		}
@@ -66,19 +69,24 @@ public class MainFrame {
 		});
 	}
 
-	//This Method must receive The results of the excel  
+	//This Method must receive The results of the excel 
+	
+	/**
+	 * @param fileResultsPanel
+	 */
 	private void addContentToFileResultsforPanel(JPanel fileResultsPanel) {
+		
 		fileResultsPanel.add(new JLabel(""));
 		fileResultsPanel.add(new JLabel("DCI"));
 		fileResultsPanel.add(new JLabel("DII"));
 		fileResultsPanel.add(new JLabel("ADCI"));
 		fileResultsPanel.add(new JLabel("ADII"));
-		fileResultsPanel.add(new JLabel("     iPlasma  "));
+		fileResultsPanel.add(new JLabel("iPlasma  "));
 		fileResultsPanel.add(new JLabel("a"));
 		fileResultsPanel.add(new JLabel("b"));
 		fileResultsPanel.add(new JLabel("c"));
 		fileResultsPanel.add(new JLabel("d"));
-		fileResultsPanel.add(new JLabel("     PMD"));
+		fileResultsPanel.add(new JLabel("PMD"));
 		fileResultsPanel.add(new JLabel("e"));
 		fileResultsPanel.add(new JLabel("f"));
 		fileResultsPanel.add(new JLabel("g"));
