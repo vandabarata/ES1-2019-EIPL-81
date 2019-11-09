2ff7069839e57ec93371a04171baf3f31c147957
lrnsa-iscteiul <lrnsa@iscte-iul.pt>
2019-11-09 15:23:43 +0000

    Added excel to GUI

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -9,61 +9,74 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.WindowConstants;
 
 public class MainFrame {
 	private JFrame mainFrame;
 	private JPanel centralPanel;
 	private JPanel southPanel;
+	private JTable excelTable;
 	private JButton add_editButton;
 	private JButton checkQualityButton;
 
-	//TODO: receber por argumento o ficheiro excel
-	public MainFrame() {
+	// TODO: receber por argumento o ficheiro excel
+	public MainFrame(JTable excelTable) {
 		mainFrame = new JFrame();
 		mainFrame.setLayout(new BorderLayout());
 		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		this.excelTable = excelTable;
+		
 		addContents();
+		
 		mainFrame.pack();
 		mainFrame.setVisible(true);
-	}
-
-	private void createCentralPanel() { 
-		centralPanel = new JPanel();
-		centralPanel.setLayout(new BorderLayout());
-		JScrollPane excelScrollPane = new JScrollPane();
-		centralPanel.add(excelScrollPane);
 	}
 
 	private void addContents() {
 		createCentralPanel();
 		mainFrame.add(centralPanel, BorderLayout.CENTER);
+		
 		createSouthPanel();
 		mainFrame.add(southPanel, BorderLayout.SOUTH);
 	}
 
+	private void createCentralPanel() {
+		centralPanel = new JPanel();
+		centralPanel.setLayout(new BorderLayout());
+		
+		JScrollPane excelScrollPane = new JScrollPane(excelTable);
+
+		centralPanel.add(excelScrollPane);
+	}
+
 	private void createSouthPanel() {
-		southPanel= new JPanel();
+		southPanel = new JPanel();
 		southPanel.setLayout(new BorderLayout());
+		
 		JPanel fileResultsPanel = new JPanel();
-		fileResultsPanel.setLayout(new GridLayout(3,5));
+		fileResultsPanel.setLayout(new GridLayout(3, 5));
+		
 		addContentToFileResultsforPanel(fileResultsPanel);
 		southPanel.add(fileResultsPanel, BorderLayout.CENTER);
+		
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new GridLayout(2,1));;
+		buttonsPanel.setLayout(new GridLayout(2, 1));
+		
 		addContentToButtonsPanel(buttonsPanel);
 		southPanel.add(buttonsPanel, BorderLayout.EAST);
-		}
+	}
 
 	private void addContentToButtonsPanel(JPanel buttonsPanel) {
 		add_editButton = new JButton("Add/ Edit Rules");
 		buttonsPanel.add(add_editButton);
-				
+
 		checkQualityButton = new JButton("Check quality");
 		buttonsPanel.add(checkQualityButton);
 	}
 
-	//This Method must receive The results of the excel  
+	// This Method must receive The results of the excel
 	private void addContentToFileResultsforPanel(JPanel fileResultsPanel) {
 		fileResultsPanel.add(new JLabel(""));
 		fileResultsPanel.add(new JLabel("DCI"));
@@ -83,7 +96,11 @@ public class MainFrame {
 	}
 
 //TODO: Usar argumento Excel
-	
+
+	public JTable getExcelTable() {
+		return excelTable;
+	}
+
 	public JButton getAdd_editButton() {
 		return add_editButton;
 	}
@@ -91,5 +108,5 @@ public class MainFrame {
 	public JButton getCheckQualityButton() {
 		return checkQualityButton;
 	}
-	
+
 }
