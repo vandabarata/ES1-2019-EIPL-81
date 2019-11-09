eba91bf30a4b3ebb2e4c464ec7fdb232054013c5
lrnsa-iscteiul <lrnsa@iscte-iul.pt>
2019-11-09 10:32:24 +0000

    Fixed problems found on review

--- src/main/java/model/ExcelRow.java
+++ src/main/java/model/ExcelRow.java
@@ -1,9 +1,13 @@
 package main.java.model;
 
-import java.util.ArrayList;
-
+/**
+ * Represents an Excel Row model
+ * 
+ * @author Lino Silva
+ *
+ */
 public class ExcelRow {
-	
+
 	private int id;
 	private String packageName;
 	private String className;
@@ -16,83 +20,129 @@ public class ExcelRow {
 	private boolean iPlasma;
 	private boolean PMD;
 	private boolean is_feature_envy;
-	
-	public ExcelRow(ArrayList<String> rowData) {
-		this.id = Integer.parseInt(rowData.get(0));
-		this.packageName = rowData.get(1);
-		this.className = rowData.get(2);
-		this.methodName = rowData.get(3);
-		this.LOC = Integer.parseInt(rowData.get(4));
-		this.CYCLO = Integer.parseInt(rowData.get(5));
-		this.ATFD = Integer.parseInt(rowData.get(6));
-		this.LAA = Float.parseFloat(rowData.get(7));
-		this.is_long_method = Boolean.parseBoolean(rowData.get(8));
-		this.iPlasma = Boolean.parseBoolean(rowData.get(9));
-		this.PMD = Boolean.parseBoolean(rowData.get(10));
-		this.is_feature_envy = Boolean.parseBoolean(rowData.get(11));
+
+	/**
+	 * Create and ExcelRow
+	 * 
+	 * @param unformatted row's data
+	 * @return row's is_feature_envy value
+	 */
+	public ExcelRow(String[] rowData) {
+		this.id = Integer.parseInt(rowData[0]);
+		this.packageName = rowData[1];
+		this.className = rowData[2];
+		this.methodName = rowData[3];
+		this.LOC = Integer.parseInt(rowData[4]);
+		this.CYCLO = Integer.parseInt(rowData[5]);
+		this.ATFD = Integer.parseInt(rowData[6]);
+		this.LAA = Float.parseFloat(rowData[7]);
+		this.is_long_method = Boolean.parseBoolean(rowData[8]);
+		this.iPlasma = Boolean.parseBoolean(rowData[9]);
+		this.PMD = Boolean.parseBoolean(rowData[10]);
+		this.is_feature_envy = Boolean.parseBoolean(rowData[11]);
 	}
-	
+
+	/**
+	 * 
+	 * @return row's id value
+	 */
 	public int getId() {
 		return id;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's package name value
+	 */
 	public String getPackageName() {
 		return packageName;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's class name value
+	 */
 	public String getClassName() {
 		return className;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's method name value
+	 */
 	public String getMethodName() {
 		return methodName;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's LOC value
+	 */
 	public int getLOC() {
 		return LOC;
 	}
+
+	/**
+	 * 
+	 * @return row's CYCLO value
+	 */
 	public int getCYCLO() {
 		return CYCLO;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's ATFD value
+	 */
 	public int getATFD() {
 		return ATFD;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's LAA value
+	 */
 	public float getLAA() {
 		return LAA;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's is_long_method value
+	 */
 	public boolean isIs_long_method() {
 		return is_long_method;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's iPlasma value
+	 */
 	public boolean isiPlasma() {
 		return iPlasma;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's PMD value
+	 */
 	public boolean isPMD() {
 		return PMD;
 	}
-	
+
+	/**
+	 * 
+	 * @return row's is_feature_envy value
+	 */
 	public boolean isIs_feature_envy() {
 		return is_feature_envy;
 	}
-	
+
 	@Override
 	public String toString() {
-		return 	"ID: " + id + 
-				"\nPackage: " + packageName +
-				"\nClass: " + className +
-				"\nMethod: " + methodName +
-				"\nLOC: " + LOC +
-				"\nCYCLO: " + CYCLO +
-				"\nATFD: " + ATFD +
-				"\nLAA: " + LAA +
-				"\nIs Long Method: " + is_long_method +
-				"\niPlasma: " + iPlasma +
-				"\nPMD: " + PMD +
-				"\nIs Feature Envy: " + is_feature_envy;
+		return "ID: " + id + "\nPackage: " + packageName + "\nClass: " + className + "\nMethod: " + methodName
+				+ "\nLOC: " + LOC + "\nCYCLO: " + CYCLO + "\nATFD: " + ATFD + "\nLAA: " + LAA + "\nIs Long Method: "
+				+ is_long_method + "\niPlasma: " + iPlasma + "\nPMD: " + PMD + "\nIs Feature Envy: " + is_feature_envy;
 	}
-	
+
 }
