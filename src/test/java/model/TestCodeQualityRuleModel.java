8d81d29a6d0cfc7304608781831ee00dd3531cac
vsfba1-iscteiul <vsfba1@iscte-iul.pt>
2019-12-01 19:58:26 +0000

    model excel importer with 100% coverage

--- src/test/java/model/TestCodeQualityRuleModel.java
+++ src/test/java/model/TestCodeQualityRuleModel.java
@@ -2,27 +2,31 @@ package test.java.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.java.model.CodeQualityRule;
 
-/**
- * @author Lino Silva
- *
- */
 class TestCodeQualityRuleModel {
+	
+	CodeQualityRule ruleDefault;
+	CodeQualityRule potatoRule;
+	
+	@BeforeEach
+	void setup() {
+		ruleDefault = new CodeQualityRule("is_long_method", "LOC > 80 && CYCLO > 10", true, false);
+		potatoRule = new CodeQualityRule("is_potato", "ATFD > 4 && LAA < 0.42", false, false);
+	}
 
 	/**
 	 * This test validates if a default CodeQualityRule is being created successfully
 	 */
 	@Test
 	void testCreationOfDefaultRule() {
-		CodeQualityRule ruleDefault = new CodeQualityRule("is_long_method", "LOC > 80 && CYCLO > 10", true, false);
-		
 		assertEquals("is_long_method", ruleDefault.getName());
 		assertEquals("LOC > 80 && CYCLO > 10", ruleDefault.getRule());
 		assertTrue(ruleDefault.isDefault());
-		assertFalse(ruleDefault.isAdvanced());
+		assertTrue(ruleDefault.isAdvanced());
 	}
 	
 	/**
@@ -30,12 +34,10 @@ class TestCodeQualityRuleModel {
 	 */
 	@Test
 	void testCreationOfNonDefaultRule() {
-		CodeQualityRule ruleNonDefault = new CodeQualityRule("is_banana", "ATFD > 4 && LAA < 0.42", false, false);
-		
-		assertEquals("is_banana", ruleNonDefault.getName());
-		assertEquals("ATFD > 4 && LAA < 0.42", ruleNonDefault.getRule());
-		assertFalse(ruleNonDefault.isDefault());
-		assertFalse(ruleNonDefault.isAdvanced());
+		assertEquals("is_potato", potatoRule.getName());
+		assertEquals("ATFD > 4 && LAA < 0.42", potatoRule.getRule());
+		assertFalse(potatoRule.isDefault());
+		assertFalse(potatoRule.isAdvanced());
 	}
 	
 	/**
@@ -44,8 +46,6 @@ class TestCodeQualityRuleModel {
 	 */
 	@Test
 	void testNameChangeDefaultRule() {
-		CodeQualityRule ruleDefault = new CodeQualityRule("is_long_method", "LOC > 80 && CYCLO > 10", true, false);
-		
 		assertEquals("is_long_method", ruleDefault.getName());
 		ruleDefault.setName("new_method");
 		assertNotEquals("new_method", ruleDefault.getName());
@@ -58,8 +58,6 @@ class TestCodeQualityRuleModel {
 	 */
 	@Test
 	void testRuleEditionDefaultRule() {
-		CodeQualityRule ruleDefault = new CodeQualityRule("is_long_method", "LOC > 80 && CYCLO > 10", true, false);
-		
 		assertEquals("LOC > 80 && CYCLO > 10", ruleDefault.getRule());
 		ruleDefault.setRule("CYCLO < 50");
 		assertNotEquals("CYCLO < 50", ruleDefault.getRule());
@@ -72,11 +70,9 @@ class TestCodeQualityRuleModel {
 	 */
 	@Test
 	void testTypeChangeDefaultRule() {
-		CodeQualityRule ruleDefault = new CodeQualityRule("is_long_method", "LOC > 80 && CYCLO > 10", true, false);
-
-		assertFalse(ruleDefault.isAdvanced());
-		ruleDefault.setIsAdvanced(true);
-		assertNotEquals(true, ruleDefault.isAdvanced());
+		assertTrue(ruleDefault.isAdvanced());
+		ruleDefault.setIsAdvanced(false);
+		assertEquals(true, ruleDefault.isAdvanced());
 	}
 	
 	/**
@@ -85,12 +81,10 @@ class TestCodeQualityRuleModel {
 	 */
 	@Test
 	void testNameChangeNonDefaultRule() {
-		CodeQualityRule ruleNonDefault = new CodeQualityRule("is_banana", "ATFD > 4 && LAA < 0.42", false, false);
-		
-		assertEquals("is_banana", ruleNonDefault.getName());
-		ruleNonDefault.setName("new_method");
-		assertEquals("new_method", ruleNonDefault.getName());
-		assertNotEquals("is_long_method", ruleNonDefault.getName());
+		assertEquals("is_potato", potatoRule.getName());
+		potatoRule.setName("new_method");
+		assertEquals("new_method", potatoRule.getName());
+		assertNotEquals("is_potato", potatoRule.getName());
 	}
 	
 	/**
@@ -99,12 +93,10 @@ class TestCodeQualityRuleModel {
 	 */
 	@Test
 	void testRuleEditionNonDefaultRule() {
-		CodeQualityRule ruleNonDefault = new CodeQualityRule("is_banana", "ATFD > 4 && LAA < 0.42", false, false);
-		
-		assertEquals("ATFD > 4 && LAA < 0.42", ruleNonDefault.getRule());
-		ruleNonDefault.setRule("CYCLO < 50");
-		assertEquals("CYCLO < 50", ruleNonDefault.getRule());
-		assertNotEquals("ATFD > 4 && LAA < 0.42", ruleNonDefault.getRule());
+		assertEquals("ATFD > 4 && LAA < 0.42", potatoRule.getRule());
+		potatoRule.setRule("CYCLO < 50");
+		assertEquals("CYCLO < 50", potatoRule.getRule());
+		assertNotEquals("ATFD > 4 && LAA < 0.42", potatoRule.getRule());
 	}
 	
 	/**
@@ -113,11 +105,18 @@ class TestCodeQualityRuleModel {
 	 */
 	@Test
 	void testTypeChangeNonDefaultRule() {
-		CodeQualityRule ruleNonDefault = new CodeQualityRule("is_banana", "ATFD > 4 && LAA < 0.42", false, false);
-
-		assertFalse(ruleNonDefault.isAdvanced());
-		ruleNonDefault.setIsAdvanced(true);
-		assertTrue(ruleNonDefault.isAdvanced());
+		assertFalse(potatoRule.isAdvanced());
+		potatoRule.setIsAdvanced(true);
+		assertTrue(potatoRule.isAdvanced());
+	}
+	
+	/**
+	 * Tests if the toString method returns the rule's name as intended
+	 */
+	@Test
+	void testToString() {
+		String potatoName = potatoRule.toString();
+		assertEquals(potatoRule.getName(), potatoName);
 	}
 
 }
--- src/test/java/model/TestExcelImporterModel.java
+++ src/test/java/model/TestExcelImporterModel.java
@@ -6,49 +6,74 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.java.model.ExcelImporter;
 
-/**
- * @author Lino Silva
- *
- */
 class TestExcelImporterModel {
 
-	private static ExcelImporter ei;
-	
-	@BeforeAll
-	static void setUp() throws Exception {
+	ExcelImporter ei;
+
+	@BeforeEach
+	void setUp() throws Exception {
 		ei = new ExcelImporter("Long-Method.xlsx");
 	}
-	
+
+	@Test
+	void testInvalidExcelImporter() {
+		ei = new ExcelImporter("potato");
+	}
+
 	/**
-	 * This test compares the expected content from the first row with the content from a row
-	 * returned with the getSingleRow() method 
+	 * This test compares the expected content from the first row with the content
+	 * from a row returned with the getSingleRow() method
 	 */
 	@Test
 	void testSingleRowContent() {
-		String[] expectedRowUnconverted = { "1", "fat", "DocumentParseFixture", "Output()", "3", "1", "0", "1", "FALSE", "FALSE",
-				"FALSE", "FALSE" };
-		
+		String[] expectedRowUnconverted = { "1", "fat", "DocumentParseFixture", "Output()", "3", "1", "0", "1", "FALSE",
+				"FALSE", "FALSE", "FALSE" };
+
 		String[] row = ei.getSingleRow(1);
-		
+
 		assertEquals(Arrays.toString(row), Arrays.toString(expectedRowUnconverted));
-		
+
 	}
-	
+
 	/**
-	 * This test compares the content from a row with a certain index from getSingleRow()
-	 * with the same index in list returned by getAllRows() method
+	 * Compares the content from a row in a certain sheet with the expected one
+	 */
+	@Test
+	void testSingleRowWithSheetIndex() {
+		String[] expectedRowUnconverted = { "1", "fat", "DocumentParseFixture", "Output()", "3", "1", "0", "1", "FALSE",
+				"FALSE", "FALSE", "FALSE" };
+
+		String[] rowContent = ei.getSingleRow(0, 1);
+
+		assertEquals(Arrays.toString(rowContent), Arrays.toString((expectedRowUnconverted)));
+	}
+
+	/**
+	 * This test compares the content from a row with a certain index from
+	 * getSingleRow() with the same index in list returned by getAllRows() method
 	 */
 	@Test
 	void testRowIndexContent() {
 		String[] row = ei.getSingleRow(1);
 
 		ArrayList<String[]> rowsList = ei.getAllRows();
-		
+
 		assertEquals(Arrays.toString(row), Arrays.toString(rowsList.get(1)));
+	}
+
+	/**
+	 * Tests if the method getAllRows is returning the expected number of rows
+	 */
+	@Test
+	void testGetAllRows() {
+		ArrayList<String[]> allRows = ei.getAllRows(0);
+		int numberOfRows = allRows.size();
+		assertEquals(421, numberOfRows);
 	}
 
 }
