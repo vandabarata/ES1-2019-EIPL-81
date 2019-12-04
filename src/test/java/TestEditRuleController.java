615ceee974e8e44f2a2952f90b30c59bd528fb1a
ftfni-iscteiul <francielefaccin@gmail.com>
2019-12-04 21:46:46 +0000

    Add javadocs to tests and clean up

--- src/test/java/TestEditRuleController.java
+++ src/test/java/TestEditRuleController.java
@@ -3,11 +3,7 @@ package test.java;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 
@@ -21,16 +17,26 @@ import main.java.controller.MainController;
 import main.java.gui.EditRulePopup;
 import main.java.model.CodeQualityRule;
 
+/**
+ * Tests the TestEditRuleController controller
+ */
 @RunWith(JUnitPlatform.class)
 class TestEditRuleController {
 
+	/** A rule to be used for tests */
 	CodeQualityRule testRule;
+	/** A default rule for long methods test */
 	CodeQualityRule testDefaultRuleLongMethod;
+	/** A default rule for feature envy test */
 	CodeQualityRule testDefaultRuleFeatureEnvy;
+	/** A EditRuleController to be used for testing */
 	EditRuleController controller;
 
+	/**
+	 * Set up default values for testing
+	 */
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
 		testRule = new CodeQualityRule("test", "LOC > 10", false, false);
 		testDefaultRuleLongMethod = new CodeQualityRule("custom_is_long_method", "LOC > 80 && CYCLO > 10", true, true);
 		testDefaultRuleFeatureEnvy = new CodeQualityRule("custom_is_feature_envy", "ATFD > 4 && LAA < 0.42", true,
@@ -38,16 +44,27 @@ class TestEditRuleController {
 
 	}
 
+	/**
+	 * Tests that can instantiate an EditRuleController with a rule to be edited
+	 */
 	@Test
 	void testEditRuleControllerWithRule() {
 		controller = new EditRuleController(testRule);
 	}
 
+	/**
+	 * Tests that can instantiate an EditRuleController with no rule
+	 */
 	@Test
 	void testEditRuleControllerNoRule() {
 		controller = new EditRuleController();
 	}
 
+	/**
+	 * Tests that can delete a regular rule from the rulesList.
+	 * 
+	 * @throws Exception If fails to delete the rule, throws an exception
+	 */
 	@Test
 	void testDeleteRule() throws Exception {
 		MainController mc = MainController.getMainControllerInstance();
@@ -62,6 +79,9 @@ class TestEditRuleController {
 		assertFalse(updatedRulesList.contains(testRule));
 	}
 
+	/**
+	 * Tests that cannot delete a default rule.
+	 */
 	@Test
 	void testDeleteDefaultRule() {
 		MainController mc = MainController.getMainControllerInstance();
@@ -76,6 +96,9 @@ class TestEditRuleController {
 		assertTrue(updatedRulesList.contains(testDefaultRuleLongMethod));
 	}
 
+	/**
+	 * Tests that cannot delete a rule that is not in the rulesList
+	 */
 	@Test
 	void testDeleteNoExistentRule() {
 		MainController mc = MainController.getMainControllerInstance();
@@ -87,6 +110,11 @@ class TestEditRuleController {
 		assertFalse(updatedRulesList.contains(testRule));
 	}
 
+	/**
+	 * Tests that can add a new rule to the rules list.
+	 * 
+	 * @throws Exception If fails to save the rule
+	 */
 	@Test
 	void testAddNewRule() throws Exception {
 		String newName = "New Rule";
@@ -104,7 +132,12 @@ class TestEditRuleController {
 		ArrayList<CodeQualityRule> updatedRulesList = mc.getRulesList();
 		assertTrue(updatedRulesList.contains(controller.getRule()));
 	}
-	
+
+	/**
+	 * Tests that can edit a rule condition and save it.
+	 * 
+	 * @throws Exception If fails to save the rule
+	 */
 	@Test
 	void testEditAndSaveRule() throws Exception {
 		String newRuleConditions = "LOC > 10";
@@ -117,7 +150,7 @@ class TestEditRuleController {
 		EditRulePopup popup = controller.getEditRulePopup();
 		popup.getAdvancedModeButton().doClick();
 		popup.getRuleTextArea().setText(newRuleConditions);
-		
+
 		controller.saveRule();
 
 		ArrayList<CodeQualityRule> updatedRulesList = mc.getRulesList();
@@ -125,8 +158,11 @@ class TestEditRuleController {
 		assertTrue(updatedRulesList.get(index).getRule().equals(newRuleConditions));
 	}
 
+	/**
+	 * Tests that cannot save a rule without a name
+	 */
 	@Test
-	void testAddNewRuleWithoutName() throws Exception {
+	void testAddNewRuleWithoutName() {
 		String newRuleConditions = "LOC > 10";
 		MainController mc = MainController.getMainControllerInstance();
 		controller = new EditRuleController();
@@ -141,6 +177,9 @@ class TestEditRuleController {
 		assertFalse(updatedRulesList.contains(controller.getRule()));
 	}
 
+	/**
+	 * Tests that cannot save a rule without rule conditions.
+	 */
 	@Test
 	void testAddNewRuleWithoutRuleCondition() {
 		String newName = "New Rule";
@@ -156,6 +195,9 @@ class TestEditRuleController {
 		assertFalse(updatedRulesList.contains(controller.getRule()));
 	}
 
+	/**
+	 * Tests that cannot save a rule with an invalid string format
+	 */
 	@Test
 	void testAddNewRuleWithInvalidFormat() {
 		String newName = "New Rule";
@@ -174,6 +216,11 @@ class TestEditRuleController {
 		assertFalse(updatedRulesList.contains(controller.getRule()));
 	}
 
+	/**
+	 * Tests that can edit a default rule's condition and save the changes.
+	 * 
+	 * @throws Exception If fails to save the rule
+	 */
 	@Test
 	void testSaveDefaultRule() throws Exception {
 		MainController mc = MainController.getMainControllerInstance();
@@ -189,9 +236,12 @@ class TestEditRuleController {
 		assertTrue(updatedRulesList.get(index).getRule().equals(editedRuleCondition));
 	}
 
+	/**
+	 * Tests that will not save a default rule if edited anything other than the
+	 * thresholds
+	 */
 	@Test
 	void testSaveDefaultRuleWithInvalidFormat() {
-		MainController mc = MainController.getMainControllerInstance();
 		controller = new EditRuleController(testDefaultRuleLongMethod);
 		String editedRuleCondition = "LAA < 80 && ATFD > 10";
 		EditRulePopup popup = controller.getEditRulePopup();
@@ -200,6 +250,10 @@ class TestEditRuleController {
 		assertThrows(Exception.class, () -> controller.saveRule());
 	}
 
+	/**
+	 * Tests that won't validate and save a default rule that doesn't have a
+	 * validation mapped to it.
+	 */
 	@Test
 	void testValidateNonMappedDefaultRule() {
 		String ruleName = "defaultRuleNameThatDoesntExist";
@@ -210,9 +264,12 @@ class TestEditRuleController {
 		assertThrows(Exception.class, () -> controller.saveRule());
 	}
 
+	/**
+	 * Tests that will convert a string with pseudo code to valid Javascript format.
+	 */
 	@Test
 	void testGetJavascriptIfStatementString() {
-		String ruleConditions = "LOC > 10 AND LAA == 20 OR LAA != 5";
+		String ruleConditions = "IF LOC > 10 AND LAA == 20 OR LAA != 5";
 		controller = new EditRuleController();
 		String testJS = controller.getJavascriptIfStatementString(ruleConditions);
 		assertEquals("LOC > 10 && LAA == 20 || LAA != 5", testJS);
