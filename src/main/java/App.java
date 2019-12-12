efe2421c3f1b47b2a191fe54e55292155abb4117
vsfba1-iscteiul <vsfba1@iscte-iul.pt>
2019-12-12 11:02:53 +0000

    improved documentation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,11 +11,17 @@ import javax.swing.plaf.basic.BasicLookAndFeel;
  * Where the main application is run. This application is organized by the MVC
  * software design pattern. This can be summarised as such:
  * <p>
- * <b>Model-View-Controller (MVC):</b> The Model is responsible for managing the
- * data of the application. It receives user input from the controller. The View
- * means presentation of the model in a particular format. The Controller
- * receives the input, optionally validates it and then passes the input to the
- * model.
+ * <b>Model-View-Controller (MVC):</b>
+ * <p>
+ * The Model is responsible for managing the data of the application. It
+ * receives user input from the controller.
+ * <p>
+ * The View means presentation of the model in a particular format.
+ * <p>
+ * The Controller receives the input, optionally validates it and then passes
+ * the input to the model. In addition to dividing the application into these
+ * components, the model-view-controller design defines the interactions between
+ * them.
  *
  */
 public class App {
